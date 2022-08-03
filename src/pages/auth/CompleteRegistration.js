490443664edbcfc841e6a6fd07434b145f4a629c
chihabeddine <chihabeddine@chihabeddines-MacBook-Pro.local>
2022-08-03 16:10:24 +0100

    complete registration usiing firebase

--- src/pages/auth/CompleteRegistration.js
+++ src/pages/auth/CompleteRegistration.js
@@ -1,42 +1,102 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { auth } from "../../firebase";
-import {toast, ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-const RegisterComplete = ({history}) => {
-    console.log(history);
-  
-  const [email, setEmail] = useState("")
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const RegisterComplete = ({ history }) => {
+  const data = {
+    email: "",
+    password: "",
+  };
+  const [inputs, setInputs] = useState(data);
+  const { email, password } = inputs;
   const handleInputs = (e) => {
-    setEmail(e.target.value)
-
-  }
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
   const handleSubmit = async (e) => {
-
-    e.preventDefault()
-    console.log(email);
-    const config = {
-      url: process.env.REACT_APP_REGISTER_REDIRECT_URL, 
-      handleCodeInApp : true
+    e.preventDefault();
+    if (!email || !password) {
+      toast.error("Email or password is missing");
+      return;
     }
-    await auth.sendSignInLinkToEmail(email, config)
-    toast.success(`Email is sent to ${email}. CLick the link to complete your registration.`)
-    window.localStorage.setItem('emailForRegistration', email)
+
+    if(password.length < 6) {
+
+      toast.error("Password must be at least 6 characters long", toast.name);
+      return;
     
-    setEmail("")
-  }
-  const registerForm = () => <form onSubmit={handleSubmit}>
-    <input   type='email' placeholder="Email address" name="email" value={email} className="form-control" onChange={handleInputs} autoFocus/>
-  <button type="submit" className="btn  btn-raised" > Register</button>
-  </form>
+    }
+    const url = window.location.href;
+
+    try {
+      // sign with the email link that is sent during step 1 of registration
+      const data = await auth.signInWithEmailLink(email, url);
+
+      if (data.user.emailVerified) {
+        // remove user from LS
+
+        window.localStorage.removeItem("emailForRegistration");
+
+        // get user id token from current user
+        let user = auth.currentUser;
+
+        // update password
+        await user.updatePassword(password);
+
+        // user token
+        const idTokenResult = await user.getIdTokenResult();
+
+        // Redux store
+
+        console.log("user", user, "idtokenResult", idTokenResult);
+        history.push("/");
+        // redirect
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    setInputs({
+      ...inputs,
+      email: window.localStorage.getItem("emailForRegistration"),
+    });
+  }, []);
+  const CompleteRegistrationForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder={email}
+        name="email"
+        value={email}
+        className="form-control"
+        autoFocus
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        value={password}
+        onChange={handleInputs}
+        className="form-control"
+        autoFocus
+      />
+      <br />
+      <button type="submit" className="btn  btn-raised">
+        COMPLETE REGISTRATION
+      </button>
+    </form>
+  );
   return (
     <div className="container p-5">
-    <div className="row">
-      <div className="col-md-6 offset-md-3">
-        <h1> Register</h1>
-      {registerForm()}
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h3>Complete Registration</h3>
+          {CompleteRegistrationForm()}
+        </div>
       </div>
-    </div>
-    <ToastContainer />
+      <ToastContainer />
     </div>
   );
 };
